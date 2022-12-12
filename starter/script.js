077de6bf02f49e624025ae03ea6108293c13332a
Mercy <116111819+MercyAz@users.noreply.github.com>
2022-12-12 22:21:19 +0000

    Further modification to the code

--- starter/script.js
+++ starter/script.js
@@ -1,18 +1,54 @@
 //set the title page to current day
 
-var CurrentDate =  moment().format('DD MMMM YYYY');
+var CurrentDate = moment().format('DD MMMM YYYY');
 $("#currentDay").text(CurrentDate);
 
-var time = moment('09', 'HH');
+console.log(CurrentDate)
 
-time.add(1, 'hours');
+var time = moment().format('h')
+console.log (time);
 
-while (time.hour()<18) {
-    console.log(time.format('ddd Do hh:mm a'));
+var timeblocks = $('.hour')
+var Userinput = $('#textarea').text('textarea.innerText').text()
+console.log(Userinput)
+for (var item of timeblocks) 
 
-    time.add(1, 'hours');}
 
-// create an event template that has three rows and eight columns
-// assign time to the first row
-//assign text area to the second row
-//assign save button to the third row
+    var hourEl = (item.innerText)
+    var number = parseInt(hourEl, Number)
+console.log(number)
+    if (number === time) 
+    {
+        $('textarea').addClass('present')}
+
+   if (number > time)
+        {$('textarea').addClass('past')}
+        
+    if (number < time) {
+         $('textarea').addClass('future')}
+
+$('#saveBtn').click(function(){localStorage.getItem('text', Userinput)})
+// var EachHour = parseInt("", Number)
+//     //  This is why you run your condition
+//     // ParseInt() - converts a string to a number
+//     // item.attr('class', 'future')
+// }
+
+// console.log(EachHour)
+// // while (hourEl < 18
+// // if ($("#hour") === time);
+// // )
+// // time.add(1, 'hours');
+
+// // while (time.hour()<18) {
+
+// //     time.add(1, 'hours');}
+
+// //  $('#container').text(time.format('ddd Do hh:mm a'))
+
+
+// // create an event template that has three rows and eight columns
+// // assign time to the first row
+// //assign text area to the second row
+// //assign save button to the third row
+// localStorage.getItem("")
